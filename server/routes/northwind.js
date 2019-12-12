c0a1356ead481188edd9ab14fc50109ec75e7bad
idanO <iojbemail@gmail.com>
2019-12-12 21:27:44 +0200

    pool

--- server/routes/northwind.js
+++ server/routes/northwind.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const db = require("../db")
+const pool = require("../db/pool")
 
 router.get("/", (req, res, next) => {
     const { CompanyName, job } = req.query
@@ -12,8 +13,29 @@ router.get("/", (req, res, next) => {
     })
 })
 
+router.get("/cities", (req, res, next) => {
+    const { shippingFee } = req.query
+    const [query, params] = getCitiesQuery(req.query)
+    console.log(query, params)
+    db.execute(query, params, (err, result) => {
+        if (err) return res.json(err)
+        return res.json(result.map(c => c.ship_city))
+    })
+})
+
+router.get("/citiesAsync", async (req, res, next) => {
+    const [query, params] = getCitiesQuery(req.query)
+    const result = await pool.execute(query, params);
+    const [first] = result;
+    console.log(first)
+    res.json(first.map(row => row.ship_city))
+})
+
 function getCustomersQuery(params) {
     return [`select * from customers where Company = ? and job_title = ?`, [...Object.values(params)]]
+}
+function getCitiesQuery(params) {
+    return [`select distinct(ship_city) from orders where shipping_fee >= ?`, [...Object.values(params)]]
 }
 
 
