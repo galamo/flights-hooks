c6fe1b28932bc5710b6027df29f908571dc07bb3
idanO <iojbemail@gmail.com>
2019-12-16 18:35:32 +0200

    dbmysql

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,34 +4,47 @@ const sessions = require("../sessions/sessions");
 const usersData = require("../data/users.json");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
+const pool = require("../db/pool")
 
 router.post("/login", (req, res, next) => {
     const { users } = usersData;
     const { email, password } = req.body
-    const userExist = users.find(user => user.email === email && user.password === password);
-    if (!userExist) return res.status(401).send("error")
-    //GENERATE SESSION
-    const token = jwt.sign({
-        userExist
-    }, process.env.SECRET, { expiresIn: '1h' });
-    console.log(token)
+    // const userExist = users.find(user => user.email === email && user.password === password);
+    // if (!userExist) return res.status(401).send("error")
+    // //GENERATE SESSION
+    // const token = jwt.sign({
+    //     userExist
+    // }, process.env.SECRET, { expiresIn: '1h' });
+    // console.log(token)
     res.json({ message: "user logged in", token })
-
 })
 
-router.post("/register", (req, res, next) => {
+router.post("/register", async (req, res, next) => {
     const { users } = usersData;
-    const { email, password } = req.body
+    const { email, password, firstName = null, lastName = null } = req.body
     //hash password
+    // if (isUserExist()) {
+
+    // }
+    const result = await pool.execute(getUserInsertionQuery(), [email, password, firstName, lastName])
 
     //ANOTHER VALIDATION (email already exist, bad request , conflict 409 )
-    fs.writeFile("./data/users.json",
-        JSON.stringify({ users: [...users, { email, password }] }), (err) => {
-            if (err) console.log(err);
-        })
-    res.json({ message: "registration completed", redirect: true })
+    // fs.writeFile("./data/users.json",
+    //     JSON.stringify({ users: [...users, { email, password }] }), (err) => {
+    //         if (err) console.log(err);
+    //     })
+    res.json(result)
 })
 
 
 
 module.exports = router;
+
+// function isUserExist() {
+//     const
+// }
+
+function getUserInsertionQuery() {
+    return "INSERT INTO `northwind`.`users` (`email`, `password`, `first_name`, `last_name`) VALUES (?,?,?,?)"
+
+}
